da48190bea0203b1b8b921dff5562eb50500eaee
aritendry <ramahafenoaritendryotiniela@gmail.com>
2024-06-12 10:39:21 +0300

    Affichage des blogs et relation des views a base.html

--- Le_blog/urls.py
+++ Le_blog/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import index
+from blog.views import index , view_blog
 
 urlpatterns = [
-    path('',index,name='index'),
     path('admin/', admin.site.urls),
+    path('',index,name='index'),
+    path('blog/',view_blog,name='view_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from blog.models import Blog
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def view_blog(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/view_blog.html', context={'blogs': blogs})
