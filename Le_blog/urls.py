7e7ab6bf314e2d97401b01e7a90dfc63e21ddbfc
aritendry <ramahafenoaritendryotiniela@gmail.com>
2024-06-12 12:38:32 +0300

    creation des views 'create_blog' et du view_blog.html

--- Le_blog/urls.py
+++ Le_blog/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import index , view_blog
+from blog.views import index , view_blog , create_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
     path('blog/',view_blog,name='view_blog'),
+    path('create_blog/',create_blog,name='create_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , HttpResponse , redirect
 from blog.models import Blog
 # Create your views here.
 def index(request):
@@ -7,3 +7,20 @@ def index(request):
 def view_blog(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/view_blog.html', context={'blogs': blogs})
+
+def create_blog(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        summary = request.POST.get('summary')
+        author = request.POST.get('author')
+
+        if title and summary and author : 
+            slug = title.lower().replace(' ','-')
+            blog = Blog(title=title, summary=summary, author=author, slug=slug)
+            blog.save()
+            return redirect('view_blog')
+        else:
+            return HttpResponse('Veuillez remplir tous les champs')
+    else:
+        return render(request, 'blog/create_blog.html')
+        
